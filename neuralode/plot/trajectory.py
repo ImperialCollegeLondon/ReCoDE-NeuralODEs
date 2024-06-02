efd5337ded99d27f7a155b8e976507409534936b
Ekin Ozturk <ekin.ozturk17@imperial.ac.uk>
2024-06-02 22:20:53 +0100

    Updated notebook with full optimisation workflow. TODO: Add better comments/documentation

--- neuralode/plot/trajectory.py
+++ neuralode/plot/trajectory.py
@@ -5,67 +5,96 @@ import numpy
 
 
 def plot_trajectory_with_reference(
-    integration_states,
-    ideal_solution_trajectory: list[
-        typing.Union[torch.Tensor, numpy.array, float]
-    ] = None,
-    axes: plt.Axes = None,
-    method_label: str = "Euler Method",
-):
-    plot_times = [i[1].cpu().item() for i in integration_states]
-    plot_states = [i[0].cpu().item() for i in integration_states]
-
+        integration_states,
+        reference_trajectory: list[typing.Union[torch.Tensor, numpy.array, float]] = None,
+        axes: plt.Axes = None,
+        method_label: str = "Euler Method",
+        ):
+    """
+    Plots the trajectory specified by given (x,t) pairs where x can be multidimensional.
+    
+    :param integration_states: A list with tuples of (x, t) pairs; x can be a multidimensional tensor
+    :param reference_trajectory: A list with reference values of x at each t specified in integration_states
+    :param axes: Optional argument to pass in plotting axes
+    :param method_label: Label for the integration states line plots
+    :return:
+    """
+    plot_states = [i[0].detach().cpu() for i in integration_states]
+    plot_times = [i[1].detach().cpu() for i in integration_states]
+    if reference_trajectory is not None:
+        ref_traj = [
+            i.detach().cpu() if torch.is_tensor(i) else i for i in reference_trajectory
+            ]
+    
     if axes is None:
-        fig = plt.figure(figsize=(12, 5), tight_layout=True)
+        fig = plt.figure(figsize = (12, 5), tight_layout = True)
         axes = []
     else:
         fig = axes[0].get_figure()
-
-    if len(axes) < 2:
+    
+    if len(axes) < 2 and reference_trajectory is not None:
         ax = fig.add_subplot(211)
         axes.append(ax)
     else:
         ax = axes[0]
-    ax.plot(
-        plot_times,
-        plot_states,
-        marker=".",
-        markersize=0.5,
-        label=method_label,
-        linestyle="--",
-    )
-    ax.set_ylabel(r"$x(t)$")
-    ax.legend()
-    if ideal_solution_trajectory is not None:
-        ax.plot(plot_times, ideal_solution_trajectory, label="Ideal Solution")
+    # If we have multiple components, we plot all of them
+    for cidx in range(plot_states[0].ravel().numel()):
+        ax.plot(
+            plot_times,
+            [i.ravel()[cidx] for i in plot_states],
+            marker = ".",
+            markersize = 4.0,
+            label = f"{method_label}:[{cidx}]",
+            linestyle = "--",
+            )
+    ax.set_ylabel(r"$\vec{x}(t)$")
+    if reference_trajectory is not None:
+        for cidx in range(ref_traj[0].ravel().numel()):
+            ax.plot(
+                plot_times,
+                [i.ravel()[cidx] for i in ref_traj],
+                label = f"Reference:[{cidx}]",
+                alpha = 0.5,
+                )
+        ax.legend()
         if len(axes) < 2:
-            ax = fig.add_subplot(212, sharex=ax)
+            ax = fig.add_subplot(212, sharex = ax)
             axes.append(ax)
         else:
             ax = axes[1]
         ax.plot(
             plot_times,
-            [abs(i - j).item() for i, j in zip(ideal_solution_trajectory, plot_states)],
-            marker="x",
-            markersize=0.5,
-            label=f"{method_label} Residual",
-        )
+            [torch.linalg.norm(i - j).item() for i, j in zip(ref_traj, plot_states)],
+            marker = "x",
+            markersize = 0.5,
+            label = f"{method_label} Residual",
+            )
         ax.set_yscale("log")
         ax.set_xlabel(r"Time $(\mathrm{s})$")
         ax.set_ylabel(r"$\Delta x$")
+        ax.legend()
+    else:
         ax.legend()
     return fig, axes
 
 
 def plot_trajectory(
-    integration_states, axes: plt.Axes = None, method_label: str = "Euler Method"
-):
+        integration_states, axes: plt.Axes = None, method_label: str = "Euler Method"
+        ):
+    """
+    Convenience function for plotting trajectories without a reference.
+    
+    :param integration_states:
+    :param axes:
+    :param method_label:
+    :return:
+    """
     if axes is None:
-        fig = plt.figure(figsize=(12, 5), tight_layout=True)
+        fig = plt.figure(figsize = (12, 5), tight_layout = True)
         axes = [fig.add_subplot(111)]
     return plot_trajectory_with_reference(
         integration_states,
-        ideal_solution_trajectory=None,
-        axes=axes,
-        method_label=method_label,
-    )
+        reference_trajectory = None,
+        axes = axes,
+        method_label = method_label,
+        )
