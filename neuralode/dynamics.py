e1fd424a0d90d6ef6e800bb4c243fe99af65da2e
Ekin Ozturk <ekin.ozturk17@imperial.ac.uk>
2024-06-22 15:41:33 +0100

    Added notebook on the implementation of the adjoint method. After implementation, two examples of its use are shown, one replicating the previous results of inferring the frequency and damping and one a toy example of a neural network.

--- neuralode/dynamics.py
+++ neuralode/dynamics.py
@@ -22,3 +22,30 @@ def exponential_fn_solution(initial_state, t, decay_constant=-1.0):
     :return: the state at time t
     """
     return initial_state * torch.exp(decay_constant * t)
+
+
+def get_simple_harmonic_oscillator_matrix(frequency, damping):
+    return torch.stack([
+        # no x term for the derivative of x as it is equal to v
+        torch.stack([torch.zeros_like(frequency), torch.ones_like(frequency)], dim = -1),
+        # first we have the omega^2 term, then the 2*zeta*omega term
+        torch.stack([-frequency ** 2, -2 * frequency * damping], dim = -1),
+        ], dim = -2)
+
+
+def simple_harmonic_oscillator(x, t, frequency, damping):
+    # The dynamics above can easily be represented as a matrix multiplication
+    # First the matrix with the corresponding terms
+    A = get_simple_harmonic_oscillator_matrix(frequency, damping)
+    # We implement the matrix multiplication using einops
+    # This is not necessarily the most efficient, but it allows
+    # us to track the exact operation without worrying about the shapes
+    # of our tensors too much
+    # You can read '...,ij,...j->...i' as:
+    #   - The first argument is a tensor with arbitrary dimensions, but
+    #   the last two of which are of interest, labelled as 'i' and 'j'
+    #   - The second argument is a tensor with arbitrary dimensions, but
+    #   the last of which is commensurate with the number of rows of the input matrix
+    #   - Take the sum of A[...,i,j]*x[...,j] over all 'j' and the output will be indexed
+    #   by 'i' in the last dimension
+    return einops.einsum(A, x, '... row col,... col->... row')
